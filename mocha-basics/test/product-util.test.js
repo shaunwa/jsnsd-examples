552139623a12364d09ed38a5bab9cae5d98581b6
shaunwassell <shaun.wassell@cbtnuggets.com>
2024-03-05 14:48:29 -0500

    Convert to Mocha chai solution

--- mocha-basics/test/product-util.test.js
+++ mocha-basics/test/product-util.test.js
@@ -1,18 +1,23 @@
-const assert = require('assert');
+const { expect } = require('chai');
 const { getProductById, createProduct } = require('../src/product-util');
 
 const products = [{ id: 1 }, { id: 2 }, { id: 3 }];
 
-console.log('The function should return the product with a matching id property');
-assert(getProductById(products, 2) === products[1]);
-assert(getProductById(products, 3) === products[2]);
-console.log('Passed!');
+describe('getProductById()', () => {
+    it('returns the product with a matching id property', () => {
+        expect(getProductById(products, 2)).to.deep.equal({ id: 2 });
+        expect(getProductById(products, 3)).to.equal(products[2]);
+    });
 
-console.log('The function should return null if there is no matching product');
-assert(getProductById(products, 4) === null);
-console.log('Passed!');
+    it('returns null if there is no matching product', () => {
+        expect(getProductById(products, 4)).to.be.null;
+    });
+})
 
-console.log('Creates a new product with the given data');
-const newProduct = createProduct(products, { name: 'Shoes', price: 10.99 });
-assert(products.includes(newProduct));
-console.log('Passed!');
+describe('createProduct()', () => {
+    it('creates a new product with the given data', () => {
+        const newProduct = createProduct(products, { name: 'Shoes', price: 10.99 });
+        expect(newProduct).to.have.property('id').that.is.a('number');
+        expect(products).to.include(newProduct);
+    });
+});
