959e0e91083a94821359350b365fe7e0d45b4c74
shaunwassell <shaun.wassell@cbtnuggets.com>
2024-03-21 17:03:03 -0400

    Exponential backoff solution

--- upstream-error-handling/auto-retry.js
+++ upstream-error-handling/auto-retry.js
@@ -1,23 +1,31 @@
 const axios = require('axios');
 
 async function requestWithRetry(method, url, payload) {
+    const maxRetries = 4;
+    let numRetries = 0;
+    let retryTime = 500;
     let shouldRetry = true;
+    let errorToReturn;
 
-    while (shouldRetry) {
+    while (shouldRetry && numRetries < maxRetries) {
         try {
             const response = await axios({ method, url, data: payload });
             return response;
         } catch (err) {
-            console.error(err);
+            errorToReturn = err;
             if (err.code !== 'ECONNREFUSED') {
                 console.error(err);
                 shouldRetry = false;
             } else {
-                console.log('Retrying in 1 second')
-                await new Promise((resolve => setTimeout(resolve, 1000)));
+                console.log(`Retrying in ${retryTime} ms`)
+                await new Promise((resolve => setTimeout(resolve, retryTime)));
+                retryTime *= 2;
+                numRetries += 1;
             }
         }
     }
+
+    throw errorToReturn;
 }
 
 module.exports = requestWithRetry;
